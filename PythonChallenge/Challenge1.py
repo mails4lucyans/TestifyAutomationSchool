1ec431fb24d996993de9cdbe186fcfcba97fe38c
mails4lucyans <109808793+mails4lucyans@users.noreply.github.com>
2022-11-23 23:12:36 +0100

    Update file

---
+++ PythonChallenge/Challenge1.py
@@ -0,0 +1,11 @@
+# Create a two-string  variable, concatenate the
+# two variables and print out the result
+
+
+var1 = "Okhakume"
+var2 = "Lucy"
+print("variable 1:", var1)
+print("variable 2:", var2)
+var = var1 + var2
+
+print("Var:", var)
